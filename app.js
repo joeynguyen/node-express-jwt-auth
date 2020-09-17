442389b60a5d9d3db27b33fd888d68f3fb029ee3
Joey Nguyen <joey@simpleltc.com>
2020-09-17 13:16:16 -0500

    Check for user token on every request.  Display header links based on logged in status

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/authRoutes');
-const { requireAuth } = require('./middleware/authMiddleware');
-
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -23,6 +22,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', requireAuth, (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
